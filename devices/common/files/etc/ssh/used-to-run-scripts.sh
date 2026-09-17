9b694a71e2c25676ecbdd4ad77909cd9a364352f
oded106e <45544837+oded106e@users.noreply.github.com>
2026-09-18 02:28:52 +0300

    Update used-to-run-scripts.sh

--- devices/common/files/etc/ssh/used-to-run-scripts.sh
+++ devices/common/files/etc/ssh/used-to-run-scripts.sh
@@ -28,7 +28,7 @@ fi
 script3="/etc/ssh/multicast-relay.py"
 if [ -f "$script3" ]; then
     log "Running script 3 in the background..."
-python3 "$script3" --interfaces br-smarthome br-open br-lan --homebrewNetifaces --ttl 255 &
+python3 "$script3" --interfaces br-smarthome br-open br-lan --homebrewNetifaces --ttl 255 --ssdpUnicastAddr 192.168.0.1 &
 
 else
     log "Script 3 not found: $script3"
